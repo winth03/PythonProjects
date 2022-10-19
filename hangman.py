e3135e483893f03cbc434e5c73bc1a846147f9ee
winth03 <66255462+winth03@users.noreply.github.com>
2022-10-19 20:15:09 +0700

    hangman and xo finished

--- hangman.py
+++ hangman.py
@@ -1,40 +1,89 @@
 import random
 
-# Hangman - Games
-words = ["Overwatch", "Valorant", "ROV"]
-lives = 5
+words = ('ant baboon badger bat bear beaver camel cat clam cobra cougar '
+         'coyote crow deer dog donkey duck eagle ferret fox frog goat '
+         'goose hawk lion lizard llama mole monkey moose mouse mule newt '
+         'otter owl panda parrot pigeon python rabbit ram rat raven '
+         'rhino salmon seal shark sheep skunk sloth snake spider '
+         'stork swan tiger toad trout turkey turtle weasel whale wolf '
+         'wombat zebra ').split()
+hangman = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+fails = 0
 word = random.choice(words).lower()
-guessed = ["_"] * len(word)
+guessed = ("_ " * len(word)).split()
+print("The category is animals")
 print("This word has", len(word), "letters.")
 print("".join(guessed))
 
-while lives > 0:
+while fails < 6:
     if "_" not in guessed:
+        print("You win!")
         break
 
     guess = input("Guess a letter: ").lower()
-    if guess in word:
-        for i in range(len(word)):
+    if guess in word and guess not in guessed:
+        i = 0
+        while i < len(word):
             if word[i] == guess:
                 guessed[i] = guess
+            i += 1
     else:
-        print(guess, "is not in the word.")
-        lives -= 1
-    if lives == 4:
-        print("  O")
-    elif lives == 3:
-        print("  O")
-        print("  |")
-    elif lives == 2:
-        print("  O")
-        print(" \|")
-    elif lives == 1:
-        print("  O")
-        print(" \|/")
-    elif lives == 0:
-        print("  O")
-        print(" \|/")
-        print(" /\\")
+        if guess in word:
+            print("You already guessed that letter.")
+        else:
+            print(guess, "is not in the word.")
+            fails += 1
+    print(hangman[fails])
+    if fails == 6:
         print("You lose!")
         print("The word was", word)
     print("".join(guessed))
--- xoxoxoxoxox/main.py
+++ xoxoxoxoxox/main.py
@@ -1,72 +1,74 @@
-class Model:
-    def __init__(self):
-        self.grid = [
-            [0,0,0],
-            [0,0,0],
-            [0,0,0]
-        ]
+# Functions
 
-class View:
-    def __init__(self, model):
-        self.grid = model.grid
 
-    def print_grid(self):
-        for i in range(3):
-            for j in range(3):
-                print("|" + ("X" if self.grid[i][j] == 1 else "O" if self.grid[i][j] == -1 else "-"), end="")
-            print("|")
+def print_grid():
+    for i in range(3):
+        for j in range(3):
+            print(
+                "|" + ("X" if grid[i][j] == 1 else "O" if grid[i][j] == -1 else "-"), end="")
+        print("|")
 
-class Controller:
-    def __init__(self, model):
-        self.grid = model.grid
-        self.turn = 1
 
-    def get_input(self):
-        return int(input("Enter action: ")) - 1
+def get_input():
+    player = input("Enter action: ")
+    if player.isnumeric():
+        return int(player) - 1
+    else:
+        return -1
 
-    def update(self):
-        while True:
-            action = self.get_input()
-            if self.grid[action//3][action%3] == 0 and action >= 0 and action <= 8:
-                self.grid[action//3][action%3] = self.turn
-                self.turn *= -1
-                break
+
+def update():
+    while True:
+        action = get_input()
+        if action >= 0 and action <= 8:
+            if grid[action//3][action % 3] == 0:
+                grid[action//3][action % 3] = turn
+                return turn * -1
             else:
                 print("Invalid action")
+        else:
+            print("Invalid action")
 
-    def check_win(self):
-        draw = True
-        for i in range(3):
-            if self.grid[i][0] == self.grid[i][1] == self.grid[i][2] != 0:
-                return self.grid[i][0]
-            if self.grid[0][i] == self.grid[1][i] == self.grid[2][i] != 0:
-                return self.grid[0][i]
-            for j in range(3):
-                if self.grid[i][j] == 0:
-                    draw = False
-        if self.grid[0][0] == self.grid[1][1] == self.grid[2][2] != 0:
-            return self.grid[0][0]
-        if self.grid[0][2] == self.grid[1][1] == self.grid[2][0] != 0:
-            return self.grid[0][2]
-        if draw:
-            return 0
+
+def check_win():
+    draw = True
+    for i in range(3):
+        if grid[i][0] == grid[i][1] == grid[i][2] != 0:
+            return grid[i][0]
+        if grid[0][i] == grid[1][i] == grid[2][i] != 0:
+            return grid[0][i]
+        for j in range(3):
+            if grid[i][j] == 0:
+                draw = False
+    if grid[0][0] == grid[1][1] == grid[2][2] != 0:
+        return grid[0][0]
+    if grid[0][2] == grid[1][1] == grid[2][0] != 0:
+        return grid[0][2]
+    if draw:
+        return 2
+    return 0
+
 
 # Init
-model = Model()
-view = View(model)
-controller = Controller(model)
+grid = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+]
+turn = 1
 
 # Main loop
 while True:
-    view.print_grid()
-    controller.update()
-    win = controller.check_win() # none, 1, -1, 0
-    if win:
-        view.print_grid()
-        print("Player " + ("X" if win == 1 else "O") + " wins!")
+    print_grid()
+    turn = update()
+    win = check_win()
+    if win != 0: # 0 = no win, 1 = X win, -1 = O win, 2 = tie
+        print_grid()
+        if win == 2:
+            print("TIE")
+        else:
+            if win == 1:
+                print("Player X wins!")
+            else:
+                print("Player O wins!")
         break
-    elif win == 0:
-        view.print_grid()
-        print("Draw!")
-        break
-    
