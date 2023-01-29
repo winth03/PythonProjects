e41433f90abdc980631f5f3e0ae41ba4d47a33ca
winth03 <nutpapop.win@gmail.com>
2023-01-29 12:24:08 +0700

    Lab6

--- test.py
+++ test.py
@@ -1,6 +1,25 @@
-string=input()
-max=0
-a=[]
-i=0
-while i<len(string):
-    
+a = """"""
+b = """"""
+
+# Check if the two strings are equal
+def check(a, b):
+    test1 = a.strip().split('\n')
+    test2 = b.strip().split('\n')
+    for i in range(len(test1)):
+        try:
+            if test1[i] != test2[i]:
+                print(f"Line {i} is different")
+                print(f"{test1[i]}\n{test2[i]}")
+                # print the difference
+                for j in range(len(test1[i])):
+                    if test1[i][j] != test2[i][j]:
+                        print(f"{test1[i][j]} != {test2[i][j]}")
+                        break
+                return False
+        except IndexError:
+            print(f"Line {i} is different")
+            print(f"{test1[i]}\n{test2[i]}")
+            return False
+    return True
+
+print(check(a, b))
