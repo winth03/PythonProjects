a92898113c190d3721b19959ea42e4d3ba6120c0
winth03 <nutpapop.win@gmail.com>
2023-03-03 23:53:16 +0700

    Lab8 Lab9

--- CircuitLab/data-edit.py
+++ CircuitLab/data-edit.py
@@ -1,26 +1,22 @@
 import pandas as pd
 import os.path as path
-import numpy as np
 
 if __name__ == "__main__":
     file = input("Enter the file name: ")
-    if path.exists(f"./lab2_data/{file}.csv"):
+    if path.exists(f"./lab_data/{file}.csv"):
         while True:
             # Show existing data
-            df = pd.read_csv(f"./lab2_data/{file}.csv")
+            df = pd.read_csv(f"./lab_data/{file}.csv")
             print(df.tail(5))
             # Get new row data
             row = ""
             for i in range(df.columns.size):
-                if df.columns[i] == "P(R2) [mW]":
-                    value = np.prod([int(val) for val in row.split(",")][1:2])
-                else:
-                    value = input(f"Enter value for {df.columns[i]}: ")
+                value = input(f"Enter value for {df.columns[i]}: ")
                 row += f"{value},"
             # Append new row to existing data
             df.loc[df.index.size] = row[:-1].split(",")
             # Save new data
-            df.to_csv(f"./lab2_data/{file}.csv", index=False)
+            df.to_csv(f"./lab_data/{file}.csv", index=False)
             # Ask user if they want to add more data
             if input("Do you want to add more data? (Enter/y, n): ") == "n": break
     else:
@@ -35,13 +31,10 @@ if __name__ == "__main__":
             # Get new row data
             row = ""
             for i in range(len(columns)):
-                if columns[i] == "P(R2) [mW]":
-                    value = np.prod([float(val) for val in row.split(",")[:-1]][1:])
-                else:
-                    value = input(f"Enter value for {columns[i]}: ")
+                value = input(f"Enter value for {columns[i]}: ")
                 row += f"{value},"
             df.loc[df.index.size] = row[:-1].split(",")
             # Save new data
-            df.to_csv(f"./lab2_data/{file}.csv", index=False)
+            df.to_csv(f"./lab_data/{file}.csv", index=False)
             # Ask user if they want to add more data
             if input("Do you want to add more data? (Enter/y, n): ") == "n": break
