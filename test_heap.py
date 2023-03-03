10388e7a81170b5735a90d80bd575d5ec9be21c4
winth03 <nutpapop.win@gmail.com>
2023-03-03 18:48:20 +0700

    test heap

---
+++ test_heap.py
@@ -0,0 +1,47 @@
+def build_max_heap(A, i=0):
+    if i >= len(A) // 2:
+        return
+    build_max_heap(A, i+1)
+    node = i
+    max = node
+    left = 2 * i + 1  # left = 2*i + 1
+    right = 2 * i + 2  # right = 2*i + 2
+    while True:
+        if left < len(A) and A[left] > A[max]:
+            max = left
+        if right < len(A) and A[right] > A[max]:
+            max = right
+        if max != node:
+            A[max], A[node] = A[node], A[max]
+            node = max
+            left = 2 * node + 1
+            right = 2 * node + 2
+            continue
+        break
+
+def heap_sort_asc(A):
+    build_max_heap(A)
+    print(A)
+    # Sorting
+    for i in range(len(A) - 1, 0, -1):
+        A[i], A[0] = A[0], A[i]
+        node = 0
+        max = node
+        left = 2 * node + 1  # left = 2*i + 1
+        right = 2 * node + 2  # right = 2*i + 2
+        while True:
+            if left < i and A[left] > A[max]:
+                max = left
+            if right < i and A[right] > A[max]:
+                max = right
+            if max != node:
+                A[max], A[node] = A[node], A[max]
+                node = max
+                left = 2 * node + 1
+                right = 2 * node + 2
+                continue
+            break
+        print(A)
+    
+A = [3, 9, 2, 1, 4, 5]
+heap_sort_asc(A)
